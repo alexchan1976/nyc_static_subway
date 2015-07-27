4c99c173a2353463853d491c91e089f24330086d
alex chan <alexchan.1976@gmail.com>
2015-07-27 19:22:25 -0400

    remove extraneous file

--- mta_data_load.py
+++ mta_data_load.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 import csv
+import ConfigParser
 
-def read_stops():
-	with open('stops.txt', 'rb') as csvfile:
+
+
+
+def read_stops(data_directory):
+	full_path = data_directory+'/stops.txt'
+	with open(full_path, 'rb') as csvfile:
 		stops = csv.reader(csvfile, delimiter=',')
 		for row in stops:
-			print 
+			sql ="INSERT INTO mta_stops ()"
 
+app_config = ConfigParser.RawConfigParser()
+app_config.read('mta_data.cfg');
 
-read_stops()
+read_dir = app_config.get('import','data_dir')
+
+read_stops(read_dir)
